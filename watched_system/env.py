0898e524ad9f61dffecad634d167bd98e04c529e
Oluwatosin Olubiyi <olubiyiontheweb@gmail.com>
2022-07-25 11:06:17 +0100

    dockerized successfully

--- watched_system/env.py
+++ watched_system/env.py
@@ -7,5 +7,4 @@ class HiddenSettings(BaseSettings):
     AWS_SERVICE: str = "es"
     AWS_ES_ACCESS_KEY_ID: str = "AKIAYPOOJNHSC7N3YGWA"
     AWS_ES_SECRET_ACCESS_KEY: str = "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"
-    ES_INDEX: str = "quodity_status_logs"
     AUTH_TOKEN: str = "@2hbr6TB*Lya%$3DNljZ^^c$!9XOm7s5siG1KUYNUbo0!w4FmR5G2mvttDlrxiAMpzPnaMtyorfQI@kqWl"
